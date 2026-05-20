8b9de89d5e789375f38c0cb754cfad84837172a4
trade <trade@users.noreply.github.com>
2026-05-20 18:34:14 +0100

    Add Go shortcode greeting for 247 Health Center

---
+++ shortcode.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("📣 Welcome to 247 Health Center!")
+    fmt.Println("247 Health Center is dedicated to care, comfort, and community.")
+    fmt.Println("This Go shortcode celebrates your commitment to accessible health services.")
+}
